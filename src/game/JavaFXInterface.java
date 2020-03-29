97eafe0b783f75f70d6e87ee3a4c6a032d46b760
Diane Hoffstetter <dhoffstetter@mac.com>
2020-03-28 17:42:46 -0700

    Updated JavaFX to handle if a room has been visited

--- src/game/JavaFXInterface.java
+++ src/game/JavaFXInterface.java
@@ -13,26 +13,26 @@ import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 public class JavaFXInterface extends Application {
-	
+
 	Room currentRoom;
 	Label description = new Label();
 	boolean foundTreasure = false;
-	
+
 	public void initializeGame() {
-		
+
 		String roomFilename = "rooms9.csv";
 		Rooms rooms = new Rooms(roomFilename);
-		
+
 		this.currentRoom = rooms.getRoomAtID(1);
 	}
-	
+
 	@Override
 	public void start(Stage primaryStage) {
-		
+
 		initializeGame();
-		
+
 		primaryStage.setTitle("Javenture");
-		
+
 		GridPane grid = new GridPane();
 		ColumnConstraints column1 = new ColumnConstraints(100);
 		ColumnConstraints column2 = new ColumnConstraints(200);
@@ -42,12 +42,12 @@ public class JavaFXInterface extends Application {
 		RowConstraints row2 = new RowConstraints(100);
 		RowConstraints row3 = new RowConstraints(100);
 		grid.getRowConstraints().addAll(row1, row2, row3);
-		
+
 		grid.setAlignment(Pos.TOP_CENTER);
 		grid.setPadding(new Insets(25, 25, 25, 25));
-		
+
 		Scene scene = new Scene(grid);
-		
+
 		Button north = new Button("North");
 		north.setOnAction(event -> {
 			northButtonPushed();
@@ -64,67 +64,56 @@ public class JavaFXInterface extends Application {
 		west.setOnAction(event -> {
 			westButtonPushed();
 		});
-		
+
 		description.setWrapText(true);
-		description.setText(currentRoom.getShortDesc());
+		description.setText(currentRoom.getLongDesc());
 		grid.add(north, 1, 0);
 		grid.add(east, 2, 1);
 		grid.add(south, 1, 2);
 		grid.add(west, 0, 1);
 		grid.add(description, 1, 1);
-				
+
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
-	
+
 	public void northButtonPushed() {
-		System.out.println("Room: "+ currentRoom.getShortDesc());
-		
 		Room nextRoom = currentRoom.getRoomAtDirection("north");
-		if(nextRoom != null) {
-			currentRoom = nextRoom;
-			description.setText(currentRoom.getShortDesc());
-			checkoutLocation();
-		}
-		System.out.println("Room: "+ currentRoom.getShortDesc());	
+		processNextRoom(nextRoom);
 	}
-	
+
 	public void eastButtonPushed() {
-		System.out.println("Room: "+ currentRoom.getShortDesc());
-		
 		Room nextRoom = currentRoom.getRoomAtDirection("east");
-		if(nextRoom != null) {
-			currentRoom = nextRoom;
-			description.setText(currentRoom.getShortDesc());
-			checkoutLocation();
-		}
-		System.out.println("Room: "+ currentRoom.getShortDesc());	
+		processNextRoom(nextRoom);
 	}
-	
+
 	public void southButtonPushed() {
-		System.out.println("Room: "+ currentRoom.getShortDesc());
-		
 		Room nextRoom = currentRoom.getRoomAtDirection("south");
-		if(nextRoom != null) {
-			currentRoom = nextRoom;
-			description.setText(currentRoom.getShortDesc());
-			checkoutLocation();
-		}
-		System.out.println("Room: "+ currentRoom.getShortDesc());	
+		processNextRoom(nextRoom);
 	}
-	
+
 	public void westButtonPushed() {
-		System.out.println("Room: "+ currentRoom.getShortDesc());
-		
 		Room nextRoom = currentRoom.getRoomAtDirection("west");
+		processNextRoom(nextRoom);
+	}
+
+	public void processNextRoom(Room nextRoom) {
+		System.out.println("Current Room: "+ currentRoom.getName());
+		
 		if(nextRoom != null) {
 			currentRoom = nextRoom;
-			description.setText(currentRoom.getShortDesc());
+			if (currentRoom.wasVisited()) {
+				description.setText(currentRoom.getShortDesc());
+			}
+			else {
+				description.setText(currentRoom.getLongDesc());
+				currentRoom.setVisited();
+			}
 			checkoutLocation();
 		}
-		System.out.println("Room: "+ currentRoom.getShortDesc());	
+		System.out.println("Next Room: "+ currentRoom.getName());		
 	}
-	
+
 	public void checkoutLocation() {
 		if (currentRoom.getId() == 3) {
 			foundTreasure = true;
@@ -133,9 +122,9 @@ public class JavaFXInterface extends Application {
 			description.setText("You Won!");
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		launch(args);
 	}
 }
